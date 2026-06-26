1f6a4108f3f9c5a91b2f6dc3faee156ce2f4a5f3
carps <meadk989@gmail.com>
2026-06-26 19:50:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# The-Council
+
+Welcome to your new project!
+
+This project was created with Aura.
---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print("Hello from Aura!")
+
+
+if __name__ == "__main__":
+    main()
